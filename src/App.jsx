467220812b69e0e64245fc44cf58ce81161480e1
Seu nome <yago_bohmcruz@hotmail.com>
2024-04-17 14:02:57 -0300

    Feat: Components, Design and Constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
-<<<<<<< HEAD
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflor-hidden">
-        <Button className="mt-10">Something</Button>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
       </div>
       <ButtonGradient />
     </>
   );
 };
-=======
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
-}
->>>>>>> c6a687d39b24ee840944710ccbc562446ee7f05d
 
 export default App;
